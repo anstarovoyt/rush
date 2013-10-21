f4cd4dce73f0621a33b7e07869af924e1cc30cf8
Andrey Hitrin <andrey.hitrin@gmail.com>
2013-10-21 21:48:30 +0600

    First Doom test: description

---
+++ proto/src/main/java/nauhchallenge/proto/Doom.java
@@ -0,0 +1,13 @@
+package nauhchallenge.proto;
+
+/**
+ * @author Andrey Hitrin
+ * @since 21.10.13
+ */
+public class Doom
+{
+    public String getDescription()
+    {
+        return "You have to fight evil martian monsters and find your way out from this awful maze!";
+    }
+}
---
+++ proto/src/test/java/nauhchallenge/proto/DoomTest.java
@@ -0,0 +1,25 @@
+package nauhchallenge.proto;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+/**
+ * @author Andrey Hitrin
+ * @since 21.10.13
+ */
+public class DoomTest
+{
+    @Test
+    public void allOk() {
+        assertThat(true, is(true));
+    }
+
+    @Test
+    public void doomGameMustHaveDescription() {
+        Doom doom = new Doom();
+        assertThat(doom.getDescription(), containsString( "monsters" ));
+    }
+}
