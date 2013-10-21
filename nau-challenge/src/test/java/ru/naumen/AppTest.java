f5caa93a99463745a7e4cc33124826c536c9abd1
Andrew Starovoyt <astarovoyt@naumen.ru>
2013-10-21 22:58:37 +0600

    changed junit version + idea import

--- nau-challenge/src/test/java/ru/naumen/AppTest.java
+++ nau-challenge/src/test/java/ru/naumen/AppTest.java
@@ -1,36 +1,17 @@
 package ru.naumen;
 
-import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest {
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
+    @Test
     public void testApp()
     {
         assertTrue( true );
