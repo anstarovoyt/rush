6bc52c9426cf9be40ca617dafcb0959860ed3390
Andrey Hitrin <andrey.hitrin@gmail.com>
2013-10-27 23:47:47 +0600

    Упрощена логика GameProvider

--- nau-challenge/src/main/java/ru/naumen/core/game/GameProvider.java
+++ nau-challenge/src/main/java/ru/naumen/core/game/GameProvider.java
@@ -8,8 +8,6 @@ import org.springframework.stereotype.Component;
 import ru.naumen.core.game.impl.Doom;
 import ru.naumen.core.game.impl.XOGame;
 
-import com.google.common.collect.Lists;
-
 /**
  * Класс предоставляет интерфейс доступа к классам существующих игр
  * @author astarovoyt
@@ -17,24 +15,8 @@ import com.google.common.collect.Lists;
 @Component
 public class GameProvider
 {
-    List<Class<? extends Game>> games = Arrays.asList(Doom.class, XOGame.class);
-
-
     public List<Game> getNewGameList()
     {
-        List<Game> result = Lists.newArrayList();
-
-        for (Class<? extends Game> game : games)
-        {
-            try
-            {
-                result.add(game.newInstance());
-            }
-            catch (InstantiationException | IllegalAccessException e)
-            {
-                throw new RuntimeException(e);
-            }
-        }
-        return result;
+        return Arrays.asList( new Doom(), new XOGame() );
     }
 }
