845dd23ddb995a3203cc02c2122a0dd0fbd51e0d
Andrew Starovoyt <astarovoyt@naumen.ru>
2013-10-31 12:07:39 +0600

    Небольшие исправления

--- nau-challenge/src/main/java/ru/naumen/core/game/impl/Shtirlitz.java
+++ nau-challenge/src/main/java/ru/naumen/core/game/impl/Shtirlitz.java
@@ -13,32 +13,19 @@ public class Shtirlitz implements Game
 
     private static final long serialVersionUID = 1L;
 
-    @Override
-    public String getShortName()
-    {
-        return "Штирлиц";
-    }
-
-    @Override
-    public String getShortDescription()
-    {
-        return "Помоги знаменитому разведчику!";
-    }
-
-    @Override
-    public String getStateRepresentation()
-    {
-        return null;
-    }
-
     private static transient final String DESCRIPTION =
             "Штирлиц как никогда близок к провалу!<br/>" +
-                            "Враги перехватили его зашифрованное сообщение:<br/>" +
-                            "[(1, 1), (1, 41), (1, 41), (1, 65), (2, 508), (1, 3), (1, 6), (1, 14), (1, 3), (1, 7), (29, 370), (1, 25), (1, 2), (1, 5), (1, 25), (1, 7), (1, 8), (1, 50), (1, 3), (1, 10), (1, 25), (1, 66), (1, 7), (23, 242), (1, 24), (1, 10), (1, 5), (1, 7), (1, 4), (1, 9), (1, 41), (1, 24), (1, 5), (1, 50), (1, 3), (1, 6), (1, 5), (1, 7), (1, 72), (1, 110), (1, 25), (1, 110), (1, 50), (1, 5), (1, 194), (1, 2), (1, 3), (1, 7), (1, 12), (1, 41), (1, 10), (1, 2), (1, 41), (1, 9), (1, 5)]<br/>" +
+                            "Враги перехватили его зашифрованное сообщение.<br/>" +
                             "Враги знают, что в сообщении присутствует фраза 'Юстас Алексу'<br/>" +
-                            "И врагов насторожило, что Штирлиц держит в книжном шкафу <a href=\"http://goo.gl/r9LsOs\">издание Анны Карениной на русском языке.</br>" +
+                            "И врагов насторожило, что Штирлиц держит в книжном шкафу <a href=\"http://goo.gl/r9LsOs\">издание Анны Карениной на русском языке.</a></br>" +
                             "Наверное, для шифрования он использовал одну из глав этой книги. А пары чисел это номер строки и позиция символа в строке.<br/><br/>" +
                             "Расшифруйте перехваченное сообщение.";
+
+    private static transient final String MESSAGE = "[(1, 1), (1, 41), (1, 41), (1, 65), (2, 508), (1, 3), (1, 6), (1, 14), (1, 3), (1, 7), (29, 370), (1, 25), (1, 2), (1, 5), (1, 25), (1, 7), (1, 8), (1, 50), (1, 3), (1, 10), (1, 25), (1, 66), (1, 7), (23, 242), (1, 24), (1, 10), (1, 5), (1, 7), (1, 4), (1, 9), (1, 41), (1, 24), (1, 5), (1, 50), (1, 3), (1, 6), (1, 5), (1, 7), (1, 72), (1, 110), (1, 25), (1, 110), (1, 50), (1, 5), (1, 194), (1, 2), (1, 3), (1, 7), (1, 12), (1, 41), (1, 10), (1, 2), (1, 41), (1, 9), (1, 5)]";
+
+    private static transient final String MSG = "Сообщение Юстас Алексу Явка провалена Высылайте доктора";
+
+    private GameState state = GameState.IN_PROGRESS;
 
     @Override
     public String getDescription() {
@@ -50,7 +37,23 @@ public class Shtirlitz implements Game
         return "shtirlitz";
     }
 
-    private static transient final String MSG = "Сообщение Юстас Алексу Явка провалена Высылайте доктора";
+    @Override
+    public String getShortDescription()
+    {
+        return "Помоги знаменитому разведчику!";
+    }
+
+    @Override
+    public String getShortName()
+    {
+        return "Штирлиц";
+    }
+
+    @Override
+    public String getStateRepresentation()
+    {
+        return MESSAGE;
+    }
 
     @Override
     public void input(String userInput)
@@ -69,16 +72,14 @@ public class Shtirlitz implements Game
         return null;
     }
 
-    private GameState state = GameState.IN_PROGRESS;
+    @Override
+    public Game resetState() {
+        return this;
+    }
 
     @Override
     public GameState state()
     {
         return state;
-    }
-
-    @Override
-    public Game resetState() {
-        return this;
     }
 }
