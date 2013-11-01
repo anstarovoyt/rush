8b8541224856b73b347ef64b2db50c2c4cf95444
Andrew Starovoyt <astarovoyt@naumen.ru>
2013-11-01 16:59:24 +0600

    fiex

--- nau-challenge/src/main/java/ru/naumen/core/game/GameProvider.java
+++ nau-challenge/src/main/java/ru/naumen/core/game/GameProvider.java
@@ -40,7 +40,7 @@ public class GameProvider
                 openGame(new Diff(), 1),
                 openGame(new Ktulhu(), 1),
                 openGame(new TrollLife(), 1),
-                closedGame(new Fifteen(), 5),
+                closedGame(new Fifteen(), 50),
                 closedGame(new Shtirlitz(), 1),
                 closedGame(new Redo(), 1),
                 closedGame(new MagicSelect(), 1));
